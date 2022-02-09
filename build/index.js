d4bf17f1af0e1092c8daa70c8a54d69952edc240
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-02-09 13:56:44 +0000

    A naive solution to extracting data from films

--- build/index.js
+++ build/index.js
@@ -11,3 +11,13 @@ let films = fs_1.default.readFileSync('films.csv', { encoding: 'utf8' }).split('
 console.log(films);
 console.log(films[0]);
 console.log(films[0][0]);
+let count = 0;
+let actor = 'Denzel Washington';
+actor = 'Mel Gibson';
+for (let film of films) {
+    // console.log(film[4]);
+    if (film[4] === actor) {
+        count++;
+    }
+}
+console.log(`${actor} appeared in ${count} films.`);
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,22 @@ import fs from 'fs';
 
 let films = fs.readFileSync('films.csv', {encoding: 'utf8' }
 ).split('\n')
-.map((row) => {
+.map((row: string): string[] => {
   return row.split(',');
 });
 
 console.log(films);
 console.log(films[0]);
 console.log(films[0][0]);
+
+let count: number = 0;
+let actor: string = 'Denzel Washington';
+actor = 'Mel Gibson';
+
+for (let film of films) {
+  // console.log(film[4]);
+  if (film[4] === actor) {
+    count++;
+  }
+}
+console.log(`${actor} appeared in ${count} films.`)
